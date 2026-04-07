e0f641d44ce9ceb9db182425ddeb54d61c0baddb
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-04-07 19:03:11 +0900

    feat: public subnet 추가

--- main.tf
+++ main.tf
@@ -26,3 +26,14 @@ resource "aws_vpc" "sandbox_vpc" {
     Name = "sandbox-vpc"
   }
 }
+
+resource "aws_subnet" "sandbox_subnet" {
+  vpc_id     = aws_vpc.sandbox_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sandbox-subnet"
+  }
+}
